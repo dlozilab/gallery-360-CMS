2d2d59fb0f892dce86bfdb7cb9f93abe19cf1bef
bongekam16@gmail.com <bongekam16@gmail.com>
2024-11-21 16:09:32 +0200

    style: updated the styles for the orders modal

--- src/components/ordersModal.jsx
+++ src/components/ordersModal.jsx
@@ -33,36 +33,65 @@ export default function OrdersModal({ invoice }) {
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
-        style={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        <div className="w3-margin w3-padding w3-light-grey" style={{ width: "60%", borderRadius: "8px" }}>
-          <h3 className="w3-text-black">
-            <FaReceipt /> Invoice #{invoice.id}
-          </h3>
+        <div
+          className=" w3-light-grey"
+          style={{ width: "60%", borderRadius: "8px" }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              backgroundColor: "#682a17",
+              padding: "2%",
+            }}
+            className="w3-margin-bottom"
+          >
+            <span className="w3-text-white w3-xlarge">
+              <FaReceipt /> Invoice ID: {invoice.id}
+            </span>
+            <span
+              onClick={handleClose}
+              className="w3-button w3-border w3-border-white w3-round-large"
+              style={{ cursor: "pointer", color: "white" }}
+            >
+              &times;
+            </span>
+          </div>
 
           {/* Customer Details */}
-          <div className="w3-margin-bottom">
+          <div className="w3-margin-bottom w3-margin-left">
             <div>
-              <FaUser className="w3-margin-right" /> <strong>Customer:</strong> {invoice.customer_contact.name}
+              <FaUser className="w3-margin-right" /> <strong>Customer:</strong>{" "}
+              {invoice.customer_contact.name}
             </div>
             <div>
-              <FaEnvelope className="w3-margin-right" /> <strong>Email:</strong> {invoice.customer_contact.email}
+              <FaEnvelope className="w3-margin-right" /> <strong>Email:</strong>{" "}
+              {invoice.customer_contact.email}
             </div>
             <div>
-              <FaMapMarkerAlt className="w3-margin-right" /> <strong>Address:</strong> 
+              <FaMapMarkerAlt className="w3-margin-right" />{" "}
+              <strong>Address:</strong>
               {`${invoice.customer_address.street_address}, ${invoice.customer_address.local_area}, ${invoice.customer_address.city}, ${invoice.customer_address.zone}, ${invoice.customer_address.code}`}
             </div>
             <div>
-              <FaCalendarAlt className="w3-margin-right" /> <strong>Date of Purchase:</strong> {new Date(invoice.dateOfPurchase).toLocaleString()}
+              <FaCalendarAlt className="w3-margin-right" />{" "}
+              <strong>Date of Purchase:</strong>{" "}
+              {new Date(invoice.dateOfPurchase).toLocaleString()}
             </div>
           </div>
 
           {/* Artworks Table */}
-
           <table className="w3-table w3-bordered">
             <thead>
-            <tr style={{ backgroundColor: '#CEB89E' }}>
-            <th>Title</th>
+              <tr style={{ backgroundColor: "#CEB89E" }}>
+                <th>Title</th>
 
                 <th>Price</th>
               </tr>
@@ -71,28 +100,46 @@ export default function OrdersModal({ invoice }) {
               {invoice.items.map((artwork, index) => (
                 <tr key={index}>
                   <td>{artwork.title}</td>
-                  <td>{`${artwork.currencySymbol} ${artwork.price.toFixed(2)}`}</td>
+                  <td>{`${artwork.currencySymbol} ${artwork.price.toFixed(
+                    2
+                  )}`}</td>
                 </tr>
               ))}
             </tbody>
-            <tfoot style={{backgroundColor:"lightgray"}}>
-            <tr>
-                  <td><strong>Delivery Fee:</strong></td>
-                  <td>{`${invoice.currencySymbol} ${invoice.deliveryFee.toFixed(2)}`}</td>
-                </tr>
-                <tr>
-                  <td><strong>Subtotal:</strong></td>
-                  <td>{`${invoice.currencySymbol} ${invoice.subtotal.toFixed(2)}`}</td>
-                </tr>
-                <tr>
-                  <td><strong>VAT ({(invoice.VAT * 100).toFixed(0)}%):</strong></td>
-                  <td>{`${invoice.currencySymbol} ${invoice.VAT_amount.toFixed(2)}`}</td>
-                </tr>
-                <tr>
-                  <td><strong>Total:</strong></td>
-                  <td>{`${invoice.currencySymbol} ${invoice.total.toFixed(2)}`}</td>
-                </tr>
-              </tfoot>
+            <tfoot style={{ backgroundColor: "lightgray" }}>
+              <tr>
+                <td>
+                  <strong>Delivery Fee:</strong>
+                </td>
+                <td>{`${invoice.currencySymbol} ${invoice.deliveryFee.toFixed(
+                  2
+                )}`}</td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>Subtotal:</strong>
+                </td>
+                <td>{`${invoice.currencySymbol} ${invoice.subtotal.toFixed(
+                  2
+                )}`}</td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>VAT ({(invoice.VAT * 100).toFixed(0)}%):</strong>
+                </td>
+                <td>{`${invoice.currencySymbol} ${invoice.VAT_amount.toFixed(
+                  2
+                )}`}</td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>Total:</strong>
+                </td>
+                <td>{`${invoice.currencySymbol} ${invoice.total.toFixed(
+                  2
+                )}`}</td>
+              </tr>
+            </tfoot>
           </table>
 
           {/* Financial Summary 
@@ -116,14 +163,15 @@ export default function OrdersModal({ invoice }) {
           </div>*/}
 
           {/* Delivery Status */}
-          <div className="w3-margin-top">
-            <FaTruck className="w3-margin-right" /> 
+          <div className="w3-margin-top w3-margin-left">
+            <FaTruck className="w3-margin-right" />
             <strong>Delivery Status:</strong> {invoice.deliveryStatus}
           </div>
 
           {/* Special Instructions */}
-          <div className="w3-margin-top w3-margin-bottom">
-            <strong>Special Instructions:</strong> {invoice.special_instructions_delivery}
+          <div className="w3-margin-top w3-margin-bottom w3-margin-left">
+            <strong>Special Instructions:</strong>{" "}
+            {invoice.special_instructions_delivery}
           </div>
         </div>
       </Modal>
