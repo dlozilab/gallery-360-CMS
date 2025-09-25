b9c8e8db7a29fb925732b73478456a20dd380e37
Dlozilab <dlozi@mlab.co.za>
2025-09-25 12:57:44 +0200

    chore: changed footer style, elements now sit space bewteen justifid

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer style={{backgroundColor:"black",color:"white",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",padding:"2%",left:"0",bottom:"0",width:"100%"}}>
+    <footer style={{backgroundColor:"black",color:"white",display:"flex",justifyContent:"space-between",alignItems:"center",padding:"1%",width:"100%"}}>
+      <p style={{fontSize:20,}}>
+        Copyright © 2024 | Gallery 360º
+      </p>
       <img
             src={require("../assets/Invert-Gallery-360-cms-logo.jpg")}
             alt="gallery 360 logo"
             style={{ width: "5%" }}
           />
-      Copyright © 2024 | Gallery 360º
     </footer>
   )
 }
